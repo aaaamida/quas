e1a212a31488b5467f43183934990c880397b091
aaaamida <aaaamida6747@gmail.com>
2024-12-29 21:10:28 +0800

    added option to toggle app theme

--- lib/settings.dart
+++ lib/settings.dart
@@ -9,15 +9,15 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+        bool appTheme = false;
+        static const WidgetStateProperty<Icon> thumbIcon = WidgetStateProperty<Icon>.fromMap(
+                <WidgetStatesConstraint, Icon>{
+                        WidgetState.selected: Icon(Icons.light_mode_rounded),
+                        WidgetState.any: Icon(Icons.dark_mode_rounded),
+                }
+        );
 
-        List<Widget> listContents() {
-                return <Widget>[
-                        Container(color: const Color(0xFF5F5286)),
-                ];
-        }
-
-        @override
-        Widget build(BuildContext context) {
+        Widget listContents(BuildContext context) {
                 // ignore: unused_local_variable
                 const pad = SizedBox(height: 8);
                 // ignore: unused_local_variable
@@ -27,16 +27,48 @@ class _SettingsPageState extends State<SettingsPage> {
                         indent: 8,
                         endIndent: 8,
                 );
-                final ScrollController scroller = ScrollController();
+                Color textColor = switch (AdaptiveTheme.of(context).mode) {
+                        AdaptiveThemeMode.dark => const Color(0xFFFFFFFF),
+                        _                      => const Color(0xFF000000),
+                };
+                Color cardBGColor = switch (AdaptiveTheme.of(context).mode) {
+                        AdaptiveThemeMode.dark => const Color.fromRGBO(200, 200, 200, 0.4),
+                        _                      => const Color(0xFFFFFFFF)
+                };
+                return ListView(
+                        padding: const EdgeInsets.all(8),
+                        children: [
+                                pad, pad, pad,
+                                Card.outlined(
+                                        color: cardBGColor,
+                                        child: ListTile(
+                                                title: Text("App theme", style: TextStyle(color: textColor)),
+                                                trailing: Switch(
+                                                        thumbIcon: thumbIcon,
+                                                        value: appTheme,
+                                                        onChanged: (bool toggle) {
+                                                                AdaptiveTheme.of(context).toggleThemeMode(useSystem: false);
+                                                                setState(() {
+                                                                        appTheme = toggle;
+                                                                });
+                                                        }
+                                                ),
+                                        ),
+                                )
+                        ],
+                );
+        }
+
+        @override
+        Widget build(BuildContext context) {
                 return Container(
                         color: switch (AdaptiveTheme.of(context).mode) {
                                 AdaptiveThemeMode.dark => const Color(0xFF4A4659),
                                 _                      => const Color(0xFFFFFFFF),
                         },
-                        child: ListView(
-                                controller: scroller,
+                        child: Padding(
                                 padding: const EdgeInsets.all(8),
-                                children: listContents(),
+                                child: listContents(context),
                         ),
                 );
         }
